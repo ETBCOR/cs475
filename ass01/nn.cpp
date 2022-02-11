9c62a8abb9e56b69b49c00f320d2e8799acb90f2
etbcor <etbcor@gmail.com>
2022-02-10 17:14:04 -0800

    working on assingment 1 more

--- ass01/nn.cpp
+++ ass01/nn.cpp
@@ -6,20 +6,25 @@
  *
  *****************************/
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 #include "mat.h"
 #include "rand.h"
 
 // ---- constants ---- //
-const double eta = 0.001;
-const double slope = 0.001;
-const double spread = 0.0;
+const double eta = 0.0001;
+const double slope = 0.0001;
 const double bias = 1.0;
-const int numPasses = 100;
+const int numPasses = 1000;
+//const double spread = 0.0;
 
 const bool debug = false;
 
+double transfer (int, double*);
+
 int main (int argc, char *argv[]) {
 
+	srand(time(NULL));
 	initRand();
 
 	// ---- read & process the input ---- //
@@ -28,10 +33,17 @@ int main (int argc, char *argv[]) {
 	int N; cin >> N;
 
 	// initialize and name arrays
-	Matrix trI = Matrix("in");
-	Matrix trO = Matrix("out");
-	Matrix tsI = Matrix("in");
-	Matrix tsO = Matrix("out");
+	Matrix trI = Matrix("in");  // training input
+	Matrix trO = Matrix("out"); // training output
+	Matrix tsI = Matrix("in");  // test input
+	Matrix tsO = Matrix("out"); // test output
+	Matrix w = Matrix("w"); // weights
+	Matrix d = Matrix("d"); // delta
+
+	Matrix x = Matrix("x"); // 
+	Matrix y = Matrix("y"); // 
+	Matrix t = Matrix("t"); // 
+	Matrix df = Matrix("df"); // diff (t - y)
 
 	// read in the input data
 	trI.read();
@@ -39,34 +51,59 @@ int main (int argc, char *argv[]) {
 	// extract the expected outputs
 	trO = trI.extract(0, N, 0, 0);
 	tsO = tsI.extract(0, N, 0, 0);
-	// truncate the data (cut out expected outputs)
-	trI = Matrix(trI.extract(0, 0, 0, N));
-	tsI = Matrix(tsI.extract(0, 0, 0, N));
 	// normalize the data
 	trI.normalize();
 	tsI.normalize();
+	// truncate the data (cut out expected outputs)
+	trI = Matrix(trI.extract(0, 0, 0, N));
+	tsI = Matrix(tsI.extract(0, 0, 0, N));
 	// add bias column vector
-	Matrix bV = Matrix(trI.numRows(), 1, 1.0);
+	Matrix bV = Matrix(trI.numRows(), 1, bias);
 	trI = trI.joinRight(bV);
-		   bV = Matrix(tsI.numRows(), 1, 1.0);
+		   bV = Matrix(tsI.numRows(), 1, bias);
 	tsI = tsI.joinRight(bV);
 
+	// settup weights vector
+	w = Matrix(trI.numCols(), trO.numCols(), 0.0).rand(-1.0, 1.0);
+	
+	// settup delta vector
+	d = Matrix(trI.numCols(), 1, 0.0);
+
 	// --- print input that was read ---- //
-	trI.print("Inputs + bias:");
+	/*trI.print("Inputs + bias:");
 	trO.print("Outputs:");
 	tsI.print("Test Inputs + bias:");
-	tsO.print("Expected Outputs:");
-
+	tsO.print("Expected Outputs:");*/
 
 	// shuffle the training data
-	trI.shuffle();
-	
+	//trI.shuffle();
+
 	// ---- main training loop ---- //
-	for (int i = 0; i < numPasses; i++) {
-		// do the stuff
+	for (int itr = 0; itr < numPasses; itr++) {	
+		int i = rand() % trI.numRows();
 
-	}	
+		x = trI.extract(i, 0, 1, 0);
+		t = trO.extract(i, 0, 1, 0);
+		y = x.dot(w);//.mapCol(&transfer);
+		df = t.sub(y); //df.print();
+		
+		d = x.Tdot(df).scalarMul(eta);
+		w = w.add(d);
 
-
+		if (/*debug*/ itr < 10 || itr > numPasses-10) {
+			printf("\nitr: %d\n", itr);
+			w.print();
+			x.print();
+			t.print();
+			y.print();
+			d.print();
+			printf("diff:\n");
+			df.print();
+		}	
+	}
 	//m.print("Tests + bias + results + diff:");
+}
+
+double transfer (int s, double *v) {
+	return 1.0 / (1.0 + exp(-slope * *v));
 }
