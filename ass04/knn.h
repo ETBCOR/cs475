0f8916d9df291b0029bf0106e777e8d77f68dd65
etbcor <etbcor@gmail.com>
2022-03-21 22:35:07 -0700

    more

---
+++ ass04/knn.h
@@ -0,0 +1,15 @@
+/*****************************
+ * By:			Ethan Corgatelli
+ * File:        pca.h
+ * Project:     Assignment 3
+ * Class:       cs475
+ * Asn. Pg:     http://marvin.cs.uidaho.edu/Teaching/CS475/pas03.pdf
+ *
+ *****************************/
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+#include "mat.h"
+#include "rand.h"
+
