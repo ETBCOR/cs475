d668ac8c28281e29649f3068328e150fbcf21375
etbcor <etbcor@gmail.com>
2022-03-28 22:34:08 -0700

    finished pas05

--- ass05/kdtree.cpp
+++ ass05/kdtree.cpp
@@ -22,13 +22,22 @@ int main (int argc, char *argv[]) {
 	//tree.printLabeledRow(&lables);
 
 //SEARCH THE KD-TREE
-	int *bestrow;
-	float *bestdist;
-//kdtree(int bestrow, int bestdistance) returns bestrow and bestdistance
-	search(&tree, bestrow, bestdist, 0, tree.numRows() - 1);
+	for (int i = 0; i < test.numRows(); i++) {
+		
+		Matrix x = test.subMatrix(i, 0, 1, 0);
+		x.print("\nFIND:", false);
+
+		int bestrow = -1;
+		float bestdist = FLT_MAX;
+		search(&tree, x, &bestrow, &bestdist, 0, tree.numRows() - 1, 0);
+
+		Matrix r(1, 1, bestdist, "r");
+		x = tree.extract(bestrow, 0, 1, 0);
+		r = r.joinRight(x);
+		r.printLabeledRow(&lables, 1, "", false);
+	}
 }
 
-// BUILD THE KD-TREE
 void build (Matrix *t, int c, int lower, int upper, int i) {
 	int size = upper-lower+1;
 	//printf("#%d (%d-%d) size: %d\n", i, lower, upper, size);
@@ -43,7 +52,7 @@ void build (Matrix *t, int c, int lower, int upper, int i) {
 	int m = size / 2.0f + lower;
 	//printf("m: %d\n", m);
 
-	c = (c == 3) ? 1 : c + 1; // increment c
+	c = (c >= 3) ? 1 : c + 1; // increment c
 
 	// call build with left side and c+1
 	//printf("do left (%d-%d):\n", lower, m - 1);
@@ -53,40 +62,58 @@ void build (Matrix *t, int c, int lower, int upper, int i) {
 	build(t, c, m + 1, upper, i + 1);
 }
 
-//SEARCH THE KD-TREE
-void search(Matrix *t, Matrix x, int *bestrow, float *bestdist, int lower, int upper) {
+void search(Matrix *t, Matrix x, int *bestrow, float *bestdist, int lower, int upper, int c) {
 	int size = upper-lower+1;
+	int m = size / 2.0f + lower;
+	c = (c >= 3) ? 1 : c + 1; // increment c
+
+	//printf("br: %d, bd: %lf, l: %d, u: %d, size: %d, m: %d, c: %d\n", *bestrow, *bestdist, lower, upper, size, m, c);
+	
 	// if leaf node
 	if (size <= 0) {
 		return;
 	} else if (size <= 1) {
-		check();
-	} else if (size <= 2)
-		// if better bestrow save that as new best node
-
+		check(t, x, bestrow, bestdist, upper);
+	}
+	else if (size <= 2) {
+		check(t, x, bestrow, bestdist, upper);
+		check(t, x, bestrow, bestdist, lower);
+	}
 	// else if parent node case
+	else {
 		// if item left of split point?
+		if (x.get(0, c-1) < t->get(m, c)) {
 			// do left then right
-				// call search(best) on left -> bestrow and best
-				// if dist(item, split point) in dimension c > best then return
-				// call search(best) on right
-	// else
-		// do right then left
-				// call search(best) on right -> bestrow and best
-				// if dist(item, split point) in dimension c > best then return
-				// call search(best) on left
-
-	// do split point (parent)
+			// call search(best) on left -> bestrow and best
+			search (t, x, bestrow, bestdist, lower, m - 1, c);
+			// if dist(item, split point) in dimension c > best then return
+			//if (check(t, x, bestrow, bestdist, m)) return;
+			if (abs(t->get(m, c) - x.get(0, c-1)) > *bestdist) return;
+			// call search(best) on right
+			search (t, x, bestrow, bestdist, m + 1, upper, c);
+		} else {
+			// do right then left
+			// call search(best) on right -> bestrow and best
+			search (t, x, bestrow, bestdist, m + 1, upper, c);
+			// if dist(item, split point) in dimension c > best then return
+			if (abs(t->get(m, c) - x.get(0, c-1)) > *bestdist) return;
+			// call search(best) on left
+			search (t, x, bestrow, bestdist, lower, m - 1, c);
+		}
+		// do split point (parent)
 		// if better bestrow save that as new bestnode
+		check(t, x, bestrow, bestdist, m);		
+	}
 }
 
 int check (Matrix *t, Matrix x, int *bestrow, float *bestdist, int r) {
-	Matrix ch(); // best
+	Matrix ck = t->subMatrix(r, 1, 1, 0);
 
-	b = 
-	x = t.subMatrix(r, 1, 1, 0);
-	if (x.dist(b) < *bestrow) {
-		
+	// if better bestrow save that as new best node
+	if (x.dist(ck) < *bestdist) {
+		*bestrow = r;
+		*bestdist = x.dist(ck);
+		return 1;
 	}
 	return 0;
 }
--- ass05/kdtree.h
+++ ass05/kdtree.h
@@ -12,7 +12,8 @@
 #include <math.h>
 #include "mat.h"
 #include "rand.h"
+#include <cfloat>
 
 void build (Matrix *t, int c, int lower, int upper, int i);
-void search (Matrix *t, Matrix x, int *bestrow, float *bestdist, int lower, int upper);
+void search (Matrix *t, Matrix x, int *bestrow, float *bestdist, int lower, int upper, int c);
 int check (Matrix *t, Matrix x, int *bestrow, float *bestdist, int r);
